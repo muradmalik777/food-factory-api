b97993ea559d131aded240951d1d080fa690f24f
Murad Malik <muradmalik7@gmail.com>
2020-12-09 23:12:37 +0500

    roster stats are now working

--- controllers/stats.controller.js
+++ controllers/stats.controller.js
@@ -3,6 +3,7 @@ const RosterModel = require("../models/roster.model");
 const ProductModel = require("../models/product.model");
 const ErrorCodes = require("../utils/errorCodes");
 const helpers = require("../utils/helpers");
+const moment = require("moment");
 
 exports.ordersWeeklystats = (req, res) => {
   OrderModel.find({ week: req.query.week })
@@ -16,10 +17,18 @@ exports.ordersWeeklystats = (req, res) => {
 };
 
 exports.productsWeeklystats = (req, res) => {
-  OrderModel.find({ week: req.query.week })
-    .then((orders) => {
-      const data = helpers.prepareOrderWeeklyStats(req.query.week, orders);
-      res.status(200).json(data);
+  const startDate = new Date(
+    moment().day("Monday").week(req.query.week).startOf("week")
+  ).getTime();
+  const endDate = new Date(
+    moment().day("Monday").week(req.query.week).endOf("week")
+  ).getTime();
+
+  ProductModel.find({
+    $and: [{ updatedAt: { $gt: startDate } }, { updatedAt: { $lte: endDate } }],
+  })
+    .then((products) => {
+      res.status(200).json(products);
     })
     .catch((e) => {
       res.status(500).json(ErrorCodes.generateError(1));
